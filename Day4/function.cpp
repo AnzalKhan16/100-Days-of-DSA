c8f1a720f7ceefc12a0b2573415dc3fc3bda31f6
AnzalKhan16 <arrowsasuke93@gmail.com>
2026-06-16 19:19:13 +0530

    function : to print name

---
+++ Day4/function.cpp
@@ -0,0 +1,10 @@
+//Function to print your name 
+#include <iostream>
+using namespace std;
+void printName() {
+    cout << "Mohammad Anzal Khan" << endl;
+}
+int main() {
+    printName();
+    return 0;
+}
