02b22914963ada54f3f468757b8f007fb0892427
AnzalKhan16 <arrowsasuke93@gmail.com>
2026-07-19 12:19:57 +0530

    Array: Find even and odd

--- Day5/arraysbasics.cpp
+++ Day5/arraysbasics.cpp
@@ -130,6 +130,34 @@ int main (){
     return 0;
 }
 
+//Count even and odd numbers
+#include<iostream>
+using namespace std;
+int main(){
 
+    int arr[5];
+    int even = 0;
+    int odd = 0; 
+
+    cout << "Enter 5 numbers:\n";
+
+    for (int i = 0; i < 5; i++){
+        cin >> arr[i];
+    }
+    
+    
+    for (int i = 0; i < 5; i++){
+       if (arr[i] % 2 == 0)
+          even++;
+        else
+          odd++;
+    }
+    
+    cout << "Even = " << even << endl;
+    cout << "Odd = " << odd << endl; 
+
+    return 0 ;
+    
+}
 
 
