fec38f80e04cf3694aeee35bce63d596cc6c8cfc
AnzalKhan16 <arrowsasuke93@gmail.com>
2026-07-19 22:41:26 +0530

    Array: secondlargest

--- Day5/arraybasics2.cpp
+++ Day5/arraybasics2.cpp
@@ -17,3 +17,25 @@ int main(){
       cout << "Element not found";
       return 0;
 }
+
+// Second largest 
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5] = {10,80,30,40,50}
+    int largest = arr[0];
+    int secondlargest = -1;
+
+    for (int i = 1 ; i < 5; i++){
+        if (arr[i] > largest){
+            secondlargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > secondlargest && arr[i] != largest){
+            secondlargest = arr[i];
+        }
+    }
+
+    cout << secondlargest;
+    return 0;
+}
