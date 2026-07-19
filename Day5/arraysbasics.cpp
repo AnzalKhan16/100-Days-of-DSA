13a423ba167c079a50cdb6842a865e6af17949f8
AnzalKhan16 <arrowsasuke93@gmail.com>
2026-07-19 10:45:56 +0530

    array:input 5 nums and print them

---
+++ Day5/arraysbasics.cpp
@@ -0,0 +1,20 @@
+//Input 5 numbers and print them
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5];
+    cout << "Enter 5 numbers:\n";
+
+    for (int i=0;i<5;i++){
+    cin >> arr[i];
+    }
+
+    cout << "Array elements are:\n";
+
+    for (int i=0;i<5;i++){
+        cout << arr[i] << ' ';
+    }
+
+    return 0;
+}
